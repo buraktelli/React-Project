0aad2ee0a131741917fc1c4e9863e0b4a005f81c
Burak <brk.telli34@gmail.com>
2021-11-22 09:42:44 +0300

    Router eklendi

--- src/layouts/Navi.jsx
+++ src/layouts/Navi.jsx
@@ -3,12 +3,14 @@ import CartSummary from './CartSummary'
 import { Button, Container, Menu } from 'semantic-ui-react'
 import SignedOut from './SignedOut'
 import SignedIn from './SignedIn'
+import { useNavigate } from "react-router-dom";
 
 export default function Navi() {
     const [isAuth, setIsAuth] = useState(true)
-
+    const navigate = useNavigate()
     function handleSignOut() {
         setIsAuth(false)
+        navigate("/")
     }
     function handleSignIn() {
         setIsAuth(true)
