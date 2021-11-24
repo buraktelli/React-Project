162fde5c1e9cbca1023c073e89b64d89b57c993b
Burak <brk.telli34@gmail.com>
2021-11-24 09:48:49 +0300

    Redux dispatch ve toastify eklendi

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router'
-import { Button, Card, Image } from 'semantic-ui-react'
+import { Button, Card } from 'semantic-ui-react'
 import ProductService from '../services/productService'
 
 export default function ProductDetail() {
@@ -12,7 +12,7 @@ export default function ProductDetail() {
             .then(result => {
                 setProduct(result.data.data)
             })
-    }, [])
+    }, []);
     return (
         <div>
             <Card.Group>
